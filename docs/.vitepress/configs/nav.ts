8b91668c54bef24ba6cf81e039ef2435be46085a
ycjtd <17336404952@163.com>
2023-10-12 16:51:18 +0800

    部署测试

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,7 +1,7 @@
 export default function getNavs() {
   return [
     { text: "🦹 前端", link: "/FrontEnd/" },
-    { text: "🐸 框架", link: "/Framework/Vue/列表最后一条数据删除处理" },
+    { text: "🐸 框架", link: "/Framework/" },
     { text: "🥷 后端", link: "/BackEnd/" },
     { text: "🥑 算法", link: "/Problem/" },
     { text: "⭐ 面经", link: "/Interview/" },
